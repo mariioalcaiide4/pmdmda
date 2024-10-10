b813971a2cf793129159370549aadac8c2dbde5f
mariioalcaiide4 <mario.alcaideperez@iesvalleinclan.es>
2024-10-10 14:27:22 +0200

    Brr

--- pmdma/app/src/main/java/com/example/pmdma/MainActivity.java
+++ pmdma/app/src/main/java/com/example/pmdma/MainActivity.java
@@ -1,12 +1,11 @@
 package com.example.pmdma;
 
 import static android.net.Uri.parse;
-
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -27,33 +26,4 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.i("EJEMPLO", "Estoy onStart");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.i("EJEMPLO", "Estoy onStop");
-
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.i("EJEMPLO", "Estoy onResume");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.i("EJEMPLO", "Estoy onDestroy");
-
-        Intent ejemplo2 = new Intent(this, MainActivity2.class);
-        ejemplo2.setData(Uri.parse("http://www.realbetisbalompie.es"));
-        startActivity(ejemplo2);
-
-    }
 }
